c6ec93e3eb92db91680e2eadd11eef531a270837
Banana-Master <tjdgh0313@gmail.com>
2025-04-24 08:45:07 +0900

    🎉 프로젝트 시작 및 구조설정

---
+++ app/(tabs)/my/_layout.tsx
@@ -0,0 +1,15 @@
+import { colors } from '@/constants';
+import { Stack } from 'expo-router';
+
+export default function MyLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.WHITE },
+      }}
+    >
+      <Stack.Screen name="my" options={{ headerShown: false, title: '내 프로필' }} />
+    </Stack>
+  );
+}
