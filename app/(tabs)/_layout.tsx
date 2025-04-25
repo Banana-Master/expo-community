c47b02a04ac5290a04d841c8416f53bd244cc63f
Banana-Master <tjdgh0313@gmail.com>
2025-04-26 07:56:34 +0900

    💄 네비게이션 아이콘 추가가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { colors } from '@/constants';
 import { Tabs } from 'expo-router';
 import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
@@ -14,18 +15,31 @@ export default function TabLayout() {
         name="index"
         options={{
           title: '홈',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={25} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: '프로필',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'person-circle' : 'person-circle-outline'}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: '설정',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'settings' : 'settings-outline'} size={25} color={color} />
+          ),
         }}
       />
     </Tabs>
