04095696920e4216316ac6f1fdce10081de48ae5
Banana-Master <tjdgh0313@gmail.com>
2025-04-26 09:27:03 +0900

    💄 인증 스크린 마크업

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,29 @@
+import { Stack, Link } from 'expo-router';
+import Foundation from '@expo/vector-icons/Foundation';
+import { colors } from '@/constants';
+
+const AuthLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerTintColor: colors.BLACK,
+        contentStyle: { backgroundColor: colors.WHITE },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: '로그인',
+          headerShown: true,
+          headerLeft: () => (
+            <Link href={'/'} replace style={{ paddingRight: 10 }}>
+              <Foundation name="home" size={28} color={'black'} />
+            </Link>
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default AuthLayout;
